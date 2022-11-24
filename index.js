94e214c3cedc268aa6830be85230650fabdbf2b2
bangsiren <bangsir61@gmail.com>
2022-11-24 11:13:54 -1100

    FIRST COMMIT

--- index.js
+++ index.js
@@ -25,25 +25,25 @@
 // == FACTORIAL ==//
 
 let fact = (n) => {
-   let number = 1;
-   for(let i =2; i<=n; i++) {
-    if(n<2) {
+  let number = 1;
+  for (let i = 2; i <= n; i++) {
+    if (n < 2) {
       number = 1
-     } else {
+    } else {
       number = number * i
-     }
-   }
+    }
+  }
   return number;
 }
 console.log(fact(5));
 
 // == PRIME NUMBER == /
 
-const isPrime = (n) =>  {
+const isPrime = (n) => {
   let number = true;
-  if(n<2){
+  if (n < 2) {
     return !number
-  }else if(n%2 == 0){
+  } else if (n % 2 == 0) {
     return !number
   } else {
     return number
@@ -53,12 +53,12 @@ console.log(isPrime(5));
 
 //== RANGE OF PRIME NUMBER ==//
 
-const prime = (n)=> {
+const prime = (n) => {
   let primeNumber = []
   let result;
-  for (let i =2; i<n; i++) {
-    if(i%2!=0) {
-       result = primeNumber.push(i);
+  for (let i = 2; i < n; i++) {
+    if (i % 2 != 0) {
+      result = primeNumber.push(i);
     }
   }
   return primeNumber;
@@ -66,22 +66,22 @@ const prime = (n)=> {
 console.log(prime(100));
 
 //== ODD NUMBER OR EVENT NUMBER ==//
-let oddOrEventNum = (n)=> {
-    if(n%2==0) {
-      return 'number is Event'
-    } else {
-      return 'number is Odd'
-    }
+let oddOrEventNum = (n) => {
+  if (n % 2 == 0) {
+    return 'number is Event'
+  } else {
+    return 'number is Odd'
+  }
 }
 console.log(oddOrEventNum(2));
 
 //==RANGE ODD NUMBER ==//
-const oddNums = (n)=> {
+const oddNums = (n) => {
   let primeNumber = []
   let event;
   let odd
-  for (let i =2; i<n; i++) {
-  if(i%2!=0) {
+  for (let i = 2; i < n; i++) {
+    if (i % 2 != 0) {
       odd = primeNumber.push(i)
     }
   }
@@ -90,13 +90,35 @@ const oddNums = (n)=> {
 
 console.log('Odd Number ', oddNums(10))
 
-// LINEAR SEARCH 
-const linearSearch = (arr, target)=> {
-   for(let i = 0; i<arr.length; i++) {
-    if(arr[i]===target) {
+// == LINEAR SEARCH == //
+const linearSearch = (arr, target) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
       return i;
-     }
-   }
-   return -1
+    }
+  }
+  return -1
 }
-console.log('LINEAR SEARCH', linearSearch([1,6,2], 2));
+console.log('LINEAR SEARCH', linearSearch([1, 6, 2], 2));
+
+// == BINARY SEARCH  == //
+
+let binarySearch = (arr, target) => {
+  let leftSide = 0;
+  let rightSide = arr.length - 1;
+  let middle = (leftSide + rightSide) / 2
+  while (leftSide <= rightSide) {
+    if (target === arr[middle]) {
+      return middle
+    }
+    if (target < arr[middle]) {
+      rightSide = middle - 1
+    } else {
+      leftSide = middle + 1
+    }
+  }
+}
+
+console.log('Binary search', binarySearch([-5, 2, 4, 6, 10], 10))
+console.log('Binary search', binarySearch([-10, -1, 3, 5, 7, 14], 7))
+console.log('Binary search', binarySearch([-10, -3, 1, 3, 5, 7, 14], 7))
