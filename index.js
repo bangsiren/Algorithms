2107c0dce3e1b7e0d72b26c7aea8cf44babb9f20
bangsiren <bangsir61@gmail.com>
2022-11-24 13:46:13 -1100

    FIRST COMMIT

--- index.js
+++ index.js
@@ -148,3 +148,19 @@ let search = (arr, target, l, r) => {
 console.log('Binary search Recur', binarySearchRecur([-5, 2, 4, 6, 10], 10));
 console.log('Binary search Recur', binarySearchRecur([-10, -1, 3, 5, 7, 14], -10));
 console.log('Binary search Recur', binarySearchRecur([-10, -3, 1, 3, 5, 7, 14], 20));
+
+// == SORTING ALGORITHMS == //
+
+// == BUBLE SORT == //
+let bubleSort = (arr) => {
+  for (let i = 0; i<=arr.length - 1; i++) {
+
+    if(arr[i]> arr[i+1]){
+      let temp = arr[i];
+      arr[i] = arr[i+1];
+      arr[i+1] = temp
+    }
+  }
+
+}
+console.log(binarySearch(20, 10, 1, 100, -10, 10, 2, 20));
