ac5883614f969255a30d5b32ebe608445177043e
bangsiren <bangsir61@gmail.com>
2022-11-25 05:03:41 -1100

    FIRST COMMIT

--- index.js
+++ index.js
@@ -189,3 +189,28 @@ let quickSort = (arr) => {
 let ar = [8, 20, -2, 4, -6]
 console.log('Quick Sort',quickSort(ar));
 
+// ==  MERGE SORT == //
+
+const mergeSort = (arr)=> {
+  if(arr.length<2){
+    return arr;
+  }
+    let mid = Math.floor((arr.length)/2);
+    let leftArr = arr.slice(0, mid);
+    let rightArr = arr.slice(mid);
+    return merge(mergeSort(leftArr), mergeSort(rightArr));
+};
+
+let merge = (leftArr, rightArr) => {
+  let sortedArr = []
+  while(leftArr.length && rightArr.length) {
+    if(leftArr[0]<=rightArr[0]){
+      sortedArr.push(leftArr.shift())
+    }else {
+      sortedArr.push(rightArr.shift())
+    }
+  }
+  return [...sortedArr, ...leftArr, ...rightArr]
+}
+let arr = [8, 20, -2, 4, -6]
+console.log('Merge Sort',mergeSort(arr));
