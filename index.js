cd45445baee88e4b02c0186f9ce1ca19418f38af
bangsiren <bangsir61@gmail.com>
2022-11-24 13:22:14 -1100

    FIRST COMMIT

--- index.js
+++ index.js
@@ -126,20 +126,26 @@ console.log('Binary search', binarySearch([-10, -3, 1, 3, 5, 7, 14], 20));
 
 
 // == BINARY SEARCH USING RECURSIVE FUNCTION == //
-let binarySearchRecur = (arr, target, l, r) => {
-  let l = 0;
-  let r = arr.length -1;
-  if(l>r){
+let search = (arr, target, l, r) => {
+  if (l > r) {
     return -1
   }
-  let middleIndex = Math.floor((l+r)/2);
-  if(arr[middleIndex] === arr) {
+  let middleIndex = Math.floor((l + r) / 2);
+  if (arr[middleIndex] === target) {
     return middleIndex
   }
-  if(target< middleIndex){
-    return binarySearchRecur(arr, target, l, middleIndex-1)
+  if (target < arr[middleIndex]) {
+    return search(arr, target, l, middleIndex - 1)
   } else {
-    return binarySearchRecur(arr, target, middleIndex+1, r)
-
+    return search(arr, target, middleIndex + 1, r)
   }
 }
+
+let binarySearchRecur = (arr, target) => {
+  return search(arr, target, 0, arr.length - 1)
+}
+
+
+console.log('Binary search Recur', binarySearchRecur([-5, 2, 4, 6, 10], 10));
+console.log('Binary search Recur', binarySearchRecur([-10, -1, 3, 5, 7, 14], -10));
+console.log('Binary search Recur', binarySearchRecur([-10, -3, 1, 3, 5, 7, 14], 20));
